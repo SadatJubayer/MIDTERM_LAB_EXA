57b4564c5cd890cc856c5cd0707575362aa1a60b
S. M. Jubayer <sadatjubayer@gmail.com>
2020-08-16 13:23:02 +0600

    init and dependencies

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// App initialization
+const app = express();
+app.set('view engine', 'ejs');
+// app.use(express.static('./public'));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'my_super_secret_key',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ status: 'Server is up & running' });
+});
+
+// Server
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log('Server is running at ', PORT);
+});
